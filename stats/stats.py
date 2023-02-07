c6c7d5c92774ba840beba7345d7d07f8af135cc4
Deyber2000 <1846@holbertonschool.com>
2023-02-06 23:06:59 -0500

    less method having in mind positive values

--- stats/stats.py
+++ stats/stats.py
@@ -4,6 +4,12 @@ class Stats:
         self.items_amount = items_amount
 
     def greater(self, value):
+        return 0 if value > self.items_amount else\
+            self.count_dict[self.items_amount] - self.count_dict[value]
+
+    def less(self, value):
         if value > self.items_amount:
+            return self.count_dict[self.items_amount]
+        elif value < 1:
             return 0
-        return self.count_dict[self.items_amount] - self.count_dict[value]
+        return self.count_dict[value-1]
