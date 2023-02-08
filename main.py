9116c464cc57855527e0a7f58bde1f3ce5c69a00
Deyber2000 <1846@holbertonschool.com>
2023-02-07 22:36:28 -0500

    add annotaded typing

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 from typeguard import typechecked
-
+from stats.stats import Stats
+from typing import Annotated
+from annotated_types import Gt
 
 
 class DataCapture:
     """
-    A class for capturing data and building statistics from it.
+    A class for capturing data and building statistics
     Methods:
-        add(n: int): Add a new data point to the records.
-        build_stats() -> Stats: Build a Stats object containing statistics about the captured data.
+        add(n: int): Add a new positive value to the records.
+        build_stats() -> Stats: Build a Stats object containing the statistics about the captured data.
     """
 
     def __init__(self):
         self.records = {}
         self.max_number = 0
 
-    def add(self, value) -> None:
+    def add(self, value: Annotated[int, Gt(0)]) -> None:
+        if value < 0:
+            raise ValueError('Work with Positive Numbers Only')
         self.max_number = value if value > self.max_number else self.max_number
         self.records[value] = self.records.get(value, 0) + 1
 
-    def build_stats(self):
+    def build_stats(self) -> Stats:
         count_dict = {}
         current_count = 0
         for i in range(self.max_number + 1):
             if i in self.records:
                 count_dict[i] = self.records[i] + current_count
                 current_count = count_dict[i]
-                print(f"{i} record and current count: {current_count}")
             else:
                 count_dict[i] = current_count
-        print(self.records)
-        return count_dict
+        return Stats(count_dict, self.max_number)
+
 
 if __name__ == '__main__':
     capture = DataCapture()
-    capture.add(3)
+    capture.add(0)
     capture.add(9)
     capture.add(3)
     capture.add(4)
     capture.add(6)
     stats = capture.build_stats()
-    print(stats)
+    print(stats.between(4, 9))
--- stats/stats.py
+++ stats/stats.py
@@ -1,18 +1,22 @@
+from typing import Annotated
+from annotated_types import Gt
+
+
 class Stats:
-    def __init__(self, count_dict, items_amount):
+    def __init__(self, count_dict: dict, items_amount: Annotated[int, Gt(0)]):
         self.count_dict = count_dict
         self.items_amount = items_amount
 
-    def greater(self, value):
+    def greater(self, value: Annotated[int, Gt(0)]):
         return 0 if value > self.items_amount else\
             self.count_dict[self.items_amount] - self.count_dict[value]
 
-    def less(self, value):
+    def less(self, value: Annotated[int, Gt(0)]):
         if value > self.items_amount:
             return self.count_dict[self.items_amount]
         elif value < 1:
             return 0
         return self.count_dict[value-1]
 
-    def between(self, low_value, high_value):
+    def between(self, low_value: Annotated[int, Gt(0)], high_value: Annotated[int, Gt(0)]):
         return self.less(high_value + 1) - self.less(low_value)
