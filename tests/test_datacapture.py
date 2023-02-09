d0793a5e87001e4f2146ca3fcdf22a7ccf4ea726
Deyber2000 <1846@holbertonschool.com>
2023-02-09 15:44:49 -0500

    return just capture object

--- tests/test_datacapture.py
+++ tests/test_datacapture.py
@@ -4,31 +4,31 @@ from typing import List
 
 
 @pytest.fixture()
-def get_data_captured() -> List:
+def get_data_captured() -> DataCapture:
     capture = DataCapture()
     capture.add(0)
     capture.add(9)
     capture.add(3)
     capture.add(4)
     capture.add(6)
-    stats = capture.build_stats()
+    capture.build_stats()
 
-    return [capture, stats]
+    return capture
 
 
 class TestDataCapture():
     """Tests related with data capture process"""
 
-    def test_add_negative(self, get_data_captured: List):
+    def test_add_negative(self, get_data_captured: DataCapture):
         """Tests that negative are not accepted"""
-        data_capture, _ = get_data_captured
+        data_capture = get_data_captured
 
         with pytest.raises(ValueError) as e_info:
             data_capture.add(-1)
 
-    def test_add_float(self, get_data_captured: List):
+    def test_add_float(self, get_data_captured: DataCapture):
         """Tests that float values are not accepted"""
-        data_capture, _ = get_data_captured
+        data_capture = get_data_captured
 
         with pytest.raises(ValueError) as e_info:
             data_capture.add(2.5)
@@ -40,7 +40,7 @@ class TestDataCapture():
         capture.add(value)
         assert capture.records.get(value) is not None
 
-    def test_amount_of_values(self, get_data_captured: List):
+    def test_amount_of_values(self, get_data_captured: DataCapture):
         """Test that values added several times are counted correctly"""
-        data_capture, _ = get_data_captured
+        data_capture = get_data_captured
         assert data_capture.records.get(0) == 1
